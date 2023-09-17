33a180ce61b256ff34d8f0b8ed516c9b945ad102
N4huel17 <jesusdsm17@gmail.com>
2023-09-17 18:43:12 -0300

    Funcionalidad (venderBici) probada

--- app.js
+++ app.js
@@ -7,10 +7,21 @@ const dhBici={
     buscarBici: function (id) {
         const encontrada= this.bicicletas.filter(bici=> bici.id===id)
         return encontrada.length>0?encontrada[0]:null;
+    },
+
+    venderBici: function (id) {
+        const vender= this.buscarBici(id);
+        if (vender) {
+           vender.vendida=true;
+           return vender
+        }
+        else{
+            return 'Bicicleta no encontrada';}
+        
     }
      
 
 };
 
+console.log(dhBici.venderBici(1111111111));
 
-console.log("La Bici encontrada es ", dhBici.buscarBici(1));
