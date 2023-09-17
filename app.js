fe7f46f185aba4627b29fb803e0f9aee2b553f88
N4huel17 <jesusdsm17@gmail.com>
2023-09-17 18:27:14 -0300

    funcionalidad de buscar por id, probada

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 const bici= require('./datosBici.js');
-console.log(bici);
+
 
 const dhBici={
     bicicletas: bici,
 
-
-
-
-
-
-
+    buscarBici: function (id) {
+        const encontrada= this.bicicletas.filter(bici=> bici.id===id)
+        return encontrada.length>0?encontrada[0]:null;
+    }
+     
 
 };
+
+
+console.log("La Bici encontrada es ", dhBici.buscarBici(1));
